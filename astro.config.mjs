b64427008af1c84719f83daefb2d4025b8b4bf5e
Mahesh Shantaram <ms@thecontrarian.in>
2025-12-31 07:38:54 +0530

    Enables C2PA content credentials integration
    Adds support for displaying and verifying C2PA content credentials on images.
    
    - Introduces a new C2PA overlay component to display credentials.
    - Adds a new API endpoint to extract C2PA data from images.
    - Implements indicators on images with content credentials.
    - Includes the C2PA logo and relevant icons.
    - Implements a screen mode toggle
    - Refactors layout components to support dynamic credential display
    
    This enhances transparency and trust in image authenticity.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://thecont1.github.io',
-  integrations: [react()]
+  integrations: [react()],
+  output: 'static',
+  adapter: node({
+    mode: 'standalone'
+  })
 });
